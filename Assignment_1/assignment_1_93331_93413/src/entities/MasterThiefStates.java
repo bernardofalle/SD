b63a248e848308602a515c56cc0bb1512118d76a
Bernardo Falé <mbfale@ua.pt>
2023-03-14 13:20:32 +0000

    Added entities states

---
+++ Assignment_1/assignment_1_93331_93413/src/entities/MasterThiefStates.java
@@ -0,0 +1,9 @@
+package entities;
+
+public enum MasterThiefStates {
+    PLANNING_THE_HEIST,
+    DECIDING_WHAT_TO_DO,
+    ASSEMBLING_A_GROUP,
+    WAITING_FOR_GROUP_ARRIVAL,
+    PRESENTING_THE_REPORT;
+}
---
+++ Assignment_1/assignment_1_93331_93413/src/entities/OrdinaryThievesStates.java
@@ -0,0 +1,9 @@
+package entities;
+
+public enum OrdinaryThievesStates {
+    CONCENTRATION_SITE,
+    CRAWLING_INWARDS,
+    AT_A_ROOM,
+    CRAWLING_OUTWARDS,
+    COLLECTION_SITE;
+}
